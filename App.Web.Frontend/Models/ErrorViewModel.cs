1fb40d19c1343b6af95a78454ab65e9f46c39146
Aijaz Bin Qasim <aijazbinqasim@hotmail.com>
2025-01-09 21:54:21 +0500

    2 Mvc new project has been added in this solution.

---
+++ App.Web.Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace App.Web.Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Web.App.Frontend/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.App.Frontend.Controllers
+{
+
+    // Attribute routing
+    [Route("shop")]
+    public class ProductController : Controller
+    {
+
+        [Route("plist")]
+        public ViewResult Products()
+        {
+            return View();
+        }
+    }
+}

0dfbac0d23ace1e3258f87e99407c8fade133294
Aijaz Bin Qasim <aijazbinqasim@hotmail.com>
2025-02-03 22:48:19 +0500

    Add javascript

---
+++ Web.App.Frontend/wwwroot/js/myScript.js
@@ -0,0 +1,10 @@
+﻿function showName() {
+
+    let result = confirm("Are you sure you want to show your name?");
+
+    if (result) {
+        let name = prompt("Please enter your name:");
+        alert(`Name is: ${name}`);
+    }
+    return;
+}
